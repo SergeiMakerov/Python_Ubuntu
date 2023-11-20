ae1b3101a37293b6e2c3454aaaa73972fffd3ee7
Sergei <makerovs@yandex.ru>
2023-11-20 23:43:21 +0700

    add4 checkers

--- checkers.py
+++ checkers.py
@@ -1,4 +1,33 @@
 import subprocess
+import paramiko
+
+
+def ssh_checkout(host, user, passwd, cmd, text, port=22):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=passwd, port=port)
+    stdin, stdout, stderr = client.exec_command(cmd)
+    exit_code = stdout.channel.recv_exit_status()
+    out = (stdout.read() + stderr.read()).decode("utf-8")
+    client.close()
+    if text in out and exit_code == 0:
+        return True
+    else:
+        return False
+
+
+def ssh_checkout_negative(host, user, passwd, cmd, text, port=22):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=passwd, port=port)
+    stdin, stdout, stderr = client.exec_command(cmd)
+    exit_code = stdout.channel.recv_exit_status()
+    out = (stdout.read() + stderr.read()).decode("utf-8")
+    client.close()
+    if text in out and exit_code != 0:
+        return True
+    else:
+        return False
 
 
 def checkout(cmd, text):
@@ -15,3 +44,14 @@ def checkout_negative(cmd, text):
         return True
     else:
         return False
+
+
+def ssh_get(host, user, passwd, cmd, port=22):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=passwd, port=port)
+    stdin, stdout, stderr = client.exec_command(cmd)
+    out = (stdout.read() + stderr.read()).decode("utf-8")
+    client.close()
+
+    return out
