8d822c151bb689741ea067717ccef38a08501d10
Sergei <makerovs@yandex.ru>
2023-11-20 23:42:06 +0700

    add4 test negative

--- test_negative.py
+++ test_negative.py
@@ -1,6 +1,5 @@
 import yaml
-from checkers import checkout_negative
-
+from checkers import ssh_checkout_negative
 
 with open('config.yaml') as f:
     data = yaml.safe_load(f)
@@ -10,9 +9,9 @@ class TestNegative:
 
     # Извлечение файлов из архива
     def test_step1(self, make_bad_arx):
-        assert checkout_negative("cd {}; 7z e bad_arx.7z -o{} -y".format(data["folder_out"], data["folder_ext"]),
+        assert ssh_checkout_negative("0.0.0.0", "user2", "2222", "cd {}; 7z e bad_arx.7z -o{} -y".format(data["folder_out"], data["folder_ext"]),
                                  "ERRORS"), "test1 FAIL"
 
     # Целостность архива
     def test_step2(self):
-        assert checkout_negative("cd {}; 7z t bad_arx.7z".format(data["folder_out"]), "ERRORS"), "test2 FAIL"
+        assert ssh_checkout_negative("0.0.0.0", "user2", "2222", "cd {}; 7z t bad_arx.7z".format(data["folder_out"]), "ERRORS"), "test2 FAIL"
